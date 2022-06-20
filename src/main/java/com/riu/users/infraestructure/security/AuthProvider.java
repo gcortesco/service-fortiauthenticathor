66211456956edbf019c926c5d934a72aed59f13c
gonzalo cortes <gcortes@riu.com>
2022-06-20 14:51:41 +0200

    añadimos el propio certificado

--- src/main/java/com/riu/users/infraestructure/security/AuthProvider.java
+++ src/main/java/com/riu/users/infraestructure/security/AuthProvider.java
@@ -1,14 +1,28 @@
 package com.riu.users.infraestructure.security;
 
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.client.RestTemplate;
 
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.client.HttpClient;
+
+import java.io.InputStream;
+import java.security.KeyStore;
 import java.time.Duration;
 
 
@@ -39,6 +53,7 @@ public class AuthProvider implements AuthenticationProvider {
 	private Boolean verifyToken(String token) {
 		RestTemplate restTemplate = (new RestTemplateBuilder()).defaultHeader("Authorization", token)
 				.setConnectTimeout(Duration.ofMillis(3000L)).build();
+		customize(restTemplate);
 		try {
 			LOGGER.debug("DEBUG VALIDATE TOKEN");
 			LOGGER.debug(token);
@@ -52,9 +67,40 @@ public class AuthProvider implements AuthenticationProvider {
 
 	}
 
+
 	@Override
 	public boolean supports(Class<?> authentication) {
 		// this class is only responsible for AuthTokenContainers
 		return AuthenticationToken.class.isAssignableFrom(authentication);
 	}
+
+	private void customize(RestTemplate restTemplate) {
+
+		final SSLContext sslContext;
+		try {
+			SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
+			KeyStore trustStore = keyStore("classpath:fortiautenticator.p12", "password");
+			sslContextBuilder.loadTrustMaterial(trustStore, new TrustSelfSignedStrategy());
+			sslContextBuilder.loadKeyMaterial(trustStore, "password".toCharArray());
+			sslContext = sslContextBuilder.build();
+
+		} catch (Exception e) {
+			throw new IllegalStateException(
+					"Failed to setup client SSL context", e
+			);
+		}
+
+		final HttpClient httpClient = HttpClients.custom().setSslcontext(sslContext).build();
+		final ClientHttpRequestFactory requestFactory =
+				new HttpComponentsClientHttpRequestFactory(httpClient);
+		restTemplate.setRequestFactory(requestFactory);
+	}
+
+	private KeyStore keyStore(String file, String password) throws Exception {
+		KeyStore keyStore = KeyStore.getInstance("PKCS12");
+		InputStream in = getClass().getResourceAsStream("/fortiautenticator.p12");
+		keyStore.load(in, password.toCharArray());
+		return keyStore;
+	}
+
 }
